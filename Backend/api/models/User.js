275dca199ec27b1a5797fef01e3d5dd7c0678391
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2018-03-06 20:19:45 +0100

    [Feature] model and API for User objects - TKDATA-61

--- Backend/api/models/User.js
+++ Backend/api/models/User.js
@@ -5,10 +5,49 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+// using bcrypt to encrypt passwords in the database
+const bcrypt = require('bcrypt');
+
 module.exports = {
-
+// UserID, CompanyID, CarID, Address, Fname, Lname, FuelDay, FuelTime, FuelNotification
   attributes: {
-
+    // UserID, if not specified in the POST, it will be autoincremented
+    UserID: {
+      type: 'integer',
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true
+    },
+    CompanyID: {
+      type: 'integer'
+    },
+    CarID: {
+      type: 'integer'
+    },
+    Address: {
+      type: 'string',
+      required: true
+    },
+    Fname: {
+      type: 'string',
+      required: true
+    },
+    Lname: {
+      type: 'string',
+      required: true
+    },
+    FueldDay: {
+      type: 'integer',
+      min: 0,
+      max: 6
+    },
+    FuelTime: {
+      // time is deprecated, use datetime
+      type: 'datetime'
+    },
+    FuelNotification: {
+      type: 'boolean',
+      defaultsTo: false
+    }
   }
 };
-
