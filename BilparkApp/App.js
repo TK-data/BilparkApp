393ac1a3339d7daa00c222e9c24d7ed4b7d291c8
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2018-03-06 22:34:15 +0100

    [Feature] Example fetch all user objects in backend, example post user and see user object posted in response - TKDATA-63, TKDATA-62

--- BilparkApp/App.js
+++ BilparkApp/App.js
@@ -1,13 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+
+// you need to manually change this IP during production. Gonna make a config file and make a production one that will point to the actual host of our backend.
+const endpoint = "http://192.168.1.101:1337/api/User";
 
 export default class App extends React.Component {
+  state = {
+    resultFetch: null,
+    resultPost: null,
+  };
+
+  componentDidMount() {
+    this._exampleGetUsers();
+  }
+
+  // make sure there are user objects in the backend before trying this huhu
+  _exampleGetUsers = async () => {
+    try {
+      let response = await fetch(endpoint);
+      let resultFetch = await response.json();
+      this.setState({resultFetch});
+    } catch(e) {
+      this.setState({resultFetch: "oh oh error"});
+    }
+  };
+
+  _examplePostUser = async () => {
+    try {
+      let response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Address: 'Krok80',
+          Fname: 'Tom',
+          Lname: 'Buttonpress',
+          FuelDay: 2,
+          CompanyID: 123123
+        }),
+      });
+      let resultPost = await response.json();
+      this.setState({resultPost});
+    } catch(e) {
+
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ScrollView>
+          {
+            // waiting for response from fetch
+            this.state.resultFetch ? (
+              <Text>{JSON.stringify(this.state.resultFetch)}</Text>
+            ) : (
+              <Text>Fetching users from {endpoint}</Text>
+            )
+          }
+          {
+            <Button
+              onPress={this._examplePostUser}
+              title="Post set user"
+              color="#841584"
+            />
+          }
+          {
+            this.state.resultPost ? (
+              <Text>{JSON.stringify(this.state.resultPost)}</Text>
+            ) : (
+              <Text>waiting for you to post a user {endpoint}</Text>
+            )
+          }
+        </ScrollView>
       </View>
     );
   }
