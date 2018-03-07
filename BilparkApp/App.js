2125170c3af286c0360edabcf96f2508f57b5162
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2018-03-07 14:41:35 +0100

    [Feautre] added api calls to service class and used them as example - TKDATA-62, TKDATA-63

--- BilparkApp/App.js
+++ BilparkApp/App.js
@@ -1,51 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 
-// you need to manually change this IP during production. Gonna make a config file and make a production one that will point to the actual host of our backend.
-const endpoint = "http://192.168.1.101:1337/api/User";
+import UserService from './src/common/UserService';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   state = {
     resultFetch: null,
     resultPost: null,
   };
 
   componentDidMount() {
-    this._exampleGetUsers();
+    UserService.getUsers().then((resultFetch) => {
+      this.setState({ resultFetch });
+    });
   }
 
-  // make sure there are user objects in the backend before trying this huhu
-  _exampleGetUsers = async () => {
-    try {
-      let response = await fetch(endpoint);
-      let resultFetch = await response.json();
-      this.setState({resultFetch});
-    } catch(e) {
-      this.setState({resultFetch: "oh oh error"});
-    }
-  };
-
-  _examplePostUser = async () => {
-    try {
-      let response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          Address: 'Krok80',
-          Fname: 'Tom',
-          Lname: 'Buttonpress',
-          FuelDay: 2,
-          CompanyID: 123123
-        }),
-      });
-      let resultPost = await response.json();
-      this.setState({resultPost});
-    } catch(e) {
-
-    }
+  handleClick() {
+    UserService.postUserExample().then((resultPost) => {
+      this.setState({ resultPost });
+    });
   }
 
   render() {
@@ -57,12 +36,12 @@ export default class App extends React.Component {
             this.state.resultFetch ? (
               <Text>{JSON.stringify(this.state.resultFetch)}</Text>
             ) : (
-              <Text>Fetching users from {endpoint}</Text>
+              <Text>Fetching users</Text>
             )
           }
           {
             <Button
-              onPress={this._examplePostUser}
+              onPress={this.handleClick}
               title="Post set user"
               color="#841584"
             />
@@ -71,7 +50,7 @@ export default class App extends React.Component {
             this.state.resultPost ? (
               <Text>{JSON.stringify(this.state.resultPost)}</Text>
             ) : (
-              <Text>waiting for you to post a user {endpoint}</Text>
+              <Text>waiting for you to post a user</Text>
             )
           }
         </ScrollView>
