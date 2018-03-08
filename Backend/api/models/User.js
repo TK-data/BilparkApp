5cfc3dac82ad652683bb94f68b835c78982e562b
Kristiane Westgård <kristianeaw@gmail.com>
2018-03-08 13:35:24 +0100

    [Tweak] Changed from type datetime to time

--- Backend/api/models/User.js
+++ Backend/api/models/User.js
@@ -42,8 +42,7 @@ module.exports = {
       max: 6
     },
     FuelTime: {
-      // time is deprecated, use datetime
-      type: 'datetime'
+      type: 'time'
     },
     FuelNotification: {
       type: 'boolean',
