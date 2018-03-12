9ccf12efa6589161dcb258a8c6f2bc935c346662
Henrik Liodden <exburn0@gmail.com>
2018-03-12 14:03:19 +0100

    [Feature] New setup of react native/redux app - TKDATA-124

--- BilparkApp/App.js
+++ BilparkApp/App.js
@@ -1,69 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import UserService from './src/common/UserService';
+import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
+import { middleware } from './src/utils/redux';
 
+const store = createStore(
+  AppReducer,
+  applyMiddleware(middleware),
+);
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  state = {
-    resultFetch: null,
-    resultPost: null,
-  };
-
-  componentDidMount() {
-    // UserService.getUsers().then((resultFetch) => {
-    //   this.setState({ resultFetch });
-    // });
-  }
-
-  handleClick() {
-    UserService.postUserExample().then((resultPost) => {
-      this.setState({ resultPost });
-    });
-  }
-
+class ReduxExampleApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView>
-          {
-            // waiting for response from fetch
-            this.state.resultFetch ? (
-              <Text>{JSON.stringify(this.state.resultFetch)}</Text>
-            ) : (
-              <Text>Fetching users</Text>
-            )
-          }
-          {
-            <Button
-              onPress={this.handleClick}
-              title="Post set user"
-              color="#841584"
-            />
-          }
-          {
-            this.state.resultPost ? (
-              <Text>{JSON.stringify(this.state.resultPost)}</Text>
-            ) : (
-              <Text>waiting for you to post a user</Text>
-            )
-          }
-        </ScrollView>
-      </View>
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('ReduxExample', () => ReduxExampleApp);
+
+export default ReduxExampleApp;
