e37591ed1f8e8a54ab6f2ae15658b474b4231189
Henrik Liodden <exburn0@gmail.com>
2018-03-14 11:02:56 +0100

    [Feature] Login component refactored as a class - TKDATA-129

--- BilparkApp/src/components/LoginScreen.js
+++ BilparkApp/src/components/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,27 +16,83 @@ const styles = StyleSheet.create({
   },
 });
 
-const LoginScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Screen A
-    </Text>
-    <Text style={styles.instructions}>
-      This is great
-    </Text>
-    <Button
-      onPress={() => navigation.dispatch({ type: 'Login' })}
-      title="Log in"
-    />
-  </View>
-);
+// const xLoginScreen = ({ navigation }) => (
+//   <View style={styles.container}>
+//     <Text style={styles.welcome}>
+//       This page posts data to server for login
+//     </Text>
+//     <TextInput
+//     />
+//     <Button
+//       onPress={() => navigation.dispatch({ type: 'Login' })}
+//       title="Log in"
+//     />
+//   </View>
+// );
 
-LoginScreen.propTypes = {
-  navigation: PropTypes.object.isRequired,
-};
+class LoginScreen extends React.Component {
+  static propTypes = {
+    navigation: PropTypes.object.isRequired,
+  };
 
-LoginScreen.navigationOptions = {
-  title: 'Log In',
-};
+  static navigationOptions = {
+    title: 'Log in',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+  }
+
+  login = () => {
+    const { email, password } = this.state;
+
+    // this.props.actions.loginRequest(email, password);
+
+
+    // the loginRequest (or userService?) will handle the request and
+    // notify the navigation about change if login is success?
+  }
+
+  render() {
+    // const navigation = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          This page posts data to server for login
+        </Text>
+        <Text value={this.state.email} />
+        <TextInput
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+          secureTextEntry
+        />
+        <Button
+          /* onPress={() => navigation.dispatch({ type: 'Login' })} */
+          onPress={this.login}
+          title="Log in"
+        />
+      </View>
+    );
+  }
+}
+
+// xLoginScreen.propTypes = {
+//   navigation: PropTypes.object.isRequired,
+// };
+//
+// xLoginScreen.navigationOptions = {
+//   title: 'Log In',
+// };
 
 export default LoginScreen;
