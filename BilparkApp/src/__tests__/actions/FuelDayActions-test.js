31dd463114d061d5aac6c194c4cff88407d1d0d1
Erling Ihlen <erling_pondus@hotmail.com>
2018-04-11 13:15:30 +0200

    [Test] made tests for reducer in reducers/auth.js

--- BilparkApp/src/__tests__/actions/FuelDayActions-test.js
+++ BilparkApp/src/__tests__/actions/FuelDayActions-test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { postFuelDayFailure, postFuelDayLoading, postFuelDaySuccess, postFuelDay } from '../actions/fuelDay';
+import { postFuelDayFailure, postFuelDayLoading, postFuelDaySuccess, postFuelDay } from '../../actions/fuelDay';
 
 const axios = require('axios');
 
