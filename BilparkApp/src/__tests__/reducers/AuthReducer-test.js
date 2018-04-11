4a0355b5592417f1a17d8ed584190102729d9744
Erling Ihlen <erling_pondus@hotmail.com>
2018-04-11 13:15:30 +0200

    [Test] made tests for reducer in reducers/auth.js

---
+++ BilparkApp/src/__tests__/reducers/AuthReducer-test.js
@@ -0,0 +1,70 @@
+import reducer from '../../reducers/auth';
+import { POST_USER_FAILURE, LOGOUT_SUCCESS, POST_USER_REQUEST, POST_USER_SUCCESS } from '../../actions/auth';
+
+describe('auth reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({ isLoggedIn: false });
+  });
+
+  it('should handle POST_USER_FAILURE', () => {
+    expect(reducer([], {
+      type: POST_USER_FAILURE,
+      hasErrored: false,
+      isLoggedIn: false,
+    })).toEqual({
+      isLoggedIn: false,
+      hasErrored: false,
+    });
+  });
+
+  it('should handle LOGOUT_SUCCESS', () => {
+    expect(reducer([], {
+      type: LOGOUT_SUCCESS,
+      isLoggedIn: false,
+    })).toEqual({
+      isLoggedIn: false,
+    });
+  });
+
+  it('should handle POST_USER_REQUEST start', () => {
+    expect(reducer([], {
+      type: POST_USER_REQUEST,
+      isLoggedIn: false,
+      isLoading: true,
+    })).toEqual({
+      isLoggedIn: false,
+      isLoading: true,
+    });
+  });
+
+  it('should handle POST_USER_REQUEST finished', () => {
+    expect(reducer([], {
+      type: POST_USER_REQUEST,
+      isLoggedIn: false,
+      isLoading: false,
+    })).toEqual({
+      isLoggedIn: false,
+      isLoading: false,
+    });
+  });
+
+  it('should handle POST_USER_SUCCESS', () => {
+    const mockUser = {
+      Email: 'ama@a.com',
+      Fname: 'Erling',
+      Lname: 'I',
+      Address: 'Krok80',
+      FuelNotification: true,
+      UserID: 3,
+      FuelDay: 0,
+    };
+    expect(reducer([], {
+      type: POST_USER_SUCCESS,
+      isLoggedIn: true,
+      user: mockUser,
+    })).toEqual({
+      isLoggedIn: true,
+      user: mockUser,
+    });
+  });
+});
