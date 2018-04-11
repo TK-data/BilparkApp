e4afa21c0a83fd85a1294186d04fe583b62803cd
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-04-11 16:13:35 +0200

    [Feature] Added UserRegisterForm render test - TKDATA-141

--- BilparkApp/src/__tests__/components/UserRegister/UserRegisterForm-test.js
+++ BilparkApp/src/__tests__/components/UserRegister/UserRegisterForm-test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import UserRegisterScreen from '../../../components/UserRegister/UserRegisterScreen';
+import UserRegisterForm from '../../../components/UserRegister/UserRegisterForm';
 
 const initialState = {
 };
@@ -11,10 +11,10 @@ const initialState = {
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-describe('Testing UserRegisterScreen', () => {
+describe('Testing UserRegisterForm', () => {
   it('Renders as expected', () => {
     const comp = shallow(
-      <UserRegisterScreen />,
+      <UserRegisterForm />,
       { context: { store: mockStore(initialState) } },
     );
     expect(comp.dive()).toMatchSnapshot();
