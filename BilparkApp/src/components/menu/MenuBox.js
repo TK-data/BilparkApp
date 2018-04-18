e856660c4989e60d07df2d712867e186f158e084
oystpoyst <oystein.hammersland@gmail.com>
2018-04-18 15:05:55 +0200

    [Tweak] Changed menu box shape and icons. Added link description and some example graphs in swiper - TKDATA-182

--- BilparkApp/src/components/menu/MenuBox.js
+++ BilparkApp/src/components/menu/MenuBox.js
@@ -1,36 +1,61 @@
 import React from 'react';
-import { StyleSheet, View, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
 import { logout } from '../../actions/auth';
 
 const window = Dimensions.get('window');
 const styles = StyleSheet.create({
+  boxContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: window.width / 40,
+    marginLeft: window.width / 40,
+
+  },
   box: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    width: window.width / 3.5,
-    height: window.height / 6,
-    margin: window.width / 50,
-    borderRadius: 5,
-
+    backgroundColor: 'white',
+    // borderStyle: 'solid',
+    // borderWidth: 2,
+    // borderColor: 'white',
+    width: window.width / 4.2,
+    height: window.height / 7.6,
+    marginLeft: window.width / 60,
+    marginRight: window.width / 60,
+    marginBottom: window.width / 50,
+    borderBottomLeftRadius: 100,
+    borderBottomRightRadius: 100,
+    borderTopLeftRadius: 100,
+    borderTopRightRadius: 100,
   },
   image: {
     width: window.width * 0.15,
     height: window.height * 0.15,
   },
+  titleText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    // marginTop: window.width / 2000,
+    marginBottom: window.width / 18,
+  },
 });
 
-const MenuBox = ({ navigate, image, routeName }) => {
+const MenuBox = ({ navigate, image, routeName, pageName }) => {
 
   return (
     <TouchableOpacity onPress={() =>
       navigate(routeName)}
     >
-      <View style={styles.box}>
-        <Image resizeMode="contain" style={styles.image} source={image} />
+      <View style={styles.boxContainer}>
+        <View style={styles.box}>
+          <Image resizeMode="contain" style={styles.image} source={image} />
+        </View>
+        <Text style={styles.titleText}>{pageName}</Text>
       </View>
     </TouchableOpacity>
   );
