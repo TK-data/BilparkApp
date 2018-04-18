21e64a2018d286df0817bc60f65380ec33b238e0
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-04-18 14:21:18 +0200

    [Feature] Added immidiate local push notification and fixed letter capitalization TKDATA-67

--- BilparkApp/src/components/fuelDay/FuelDayForm.js
+++ BilparkApp/src/components/fuelDay/FuelDayForm.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { Notifications, Permissions, Constants } from 'expo';
+// import moment from 'moment';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Picker, CheckBox, ListItem, Body } from 'native-base';
 import { reduxForm, Field } from 'redux-form';
 import { postFuelDay } from '../../actions/fuelDay';
+
 
 const renderPicker = ({ func, input, label, children, ...custom }) => (
   <Picker
@@ -40,46 +43,75 @@ const renderCheckbox = ({ func, input, ...custom }) => (
 
 
 class FuelDayForm extends Component {
-
-
-  render() {
-    const { user } = this.props;
-
-    const Item = Picker.Item;
-
-    const postToggle = (value) => {
-      this.props.postFuelDay(user.FuelDay, value);
-    };
-
-    const postWeekday = (value) => {
-      this.props.postFuelDay(value, user.FuelNotification);
-    };
-
-    return (
-      <View style={styles.container}>
-        <Text>Current day: {user.FuelDay} Current value: {user.FuelNotification.toString()} </Text>
-        <Text>Velg dag og om du ønsker notification</Text>
-        <Field
-          func={postWeekday}
-          selectedValue={user.FuelDay}
-          placeholder="velg dag"
-          name="weekday"
-          component={renderPicker}
-          iosHeader="Velg dag"
-          mode="dropdown"
-        >
-          <Item label="mandag" value={0} />
-          <Item label="tirsdag" value={1} />
-          <Item label="onsdag" value={2} />
-          <Item label="torsdag" value={3} />
-          <Item label="fredag" value={4} />
-          <Item label="lørdag" value={5} />
-          <Item label="søndag" value={6} />
-        </Field>
-        <Field name="toggle" checked={user.FuelNotification} func={postToggle} component={renderCheckbox} />
-      </View>
-    );
+  async componentDidMount() {
+    const result = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (Constants.isDevice && result.status === 'granted') {
+      console.log('Notification permissions granted.');
+    }
+    Notifications.addListener(this.handleNotification);
   }
+
+  // Private methods
+
+  handleNotification = ({ origin, data }) => {
+    console.info(`Notification (${origin}) with data: ${JSON.stringify(data)}`);
+  };
+
+ sendImmediateNotification = () => {
+   const localNotification = {
+     title: 'Superdupertestingnotification',
+     body: 'Trykk på meg for å åpne den beste appen på den beste siden',
+     data: { type: 'immediate' },
+   };
+
+   console.log('Scheduling immediate notification:', { localNotification });
+
+   Notifications.presentLocalNotificationAsync(localNotification)
+     .then(id => console.info(`Immediate notification scheduled (${id})`))
+     .catch(err => console.error(err));
+ };
+
+
+ render() {
+   const { user } = this.props;
+
+   const Item = Picker.Item;
+
+   const postToggle = (value) => {
+     this.props.postFuelDay(user.FuelDay, value);
+   };
+
+   const postWeekday = (value) => {
+     this.props.postFuelDay(value, user.FuelNotification);
+   };
+
+   return (
+     <View style={styles.container}>
+       <Button title="Send Immediate Notification" onPress={() => this.sendImmediateNotification()} />
+       <Button title="Send Delayed Notification" onPress={() => this.sendDelayedNotification()} />
+       <Text>Current day: {user.FuelDay} Current value: {user.FuelNotification.toString()} </Text>
+       <Text>Velg dag og om du ønsker notification</Text>
+       <Field
+         func={postWeekday}
+         selectedValue={user.FuelDay}
+         placeholder="Velg dag"
+         name="weekday"
+         component={renderPicker}
+         iosHeader="Velg dag"
+         mode="dropdown"
+       >
+         <Item label="Mandag" value={0} />
+         <Item label="Tirsdag" value={1} />
+         <Item label="Onsdag" value={2} />
+         <Item label="Torsdag" value={3} />
+         <Item label="Fredag" value={4} />
+         <Item label="Lørdag" value={5} />
+         <Item label="Søndag" value={6} />
+       </Field>
+       <Field name="toggle" checked={user.FuelNotification} func={postToggle} component={renderCheckbox} />
+     </View>
+   );
+ }
 }
 
 const mapStateToProps = (state) => {
