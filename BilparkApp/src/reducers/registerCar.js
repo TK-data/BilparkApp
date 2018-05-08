cb882a65e3aa681f4dba6f7b713c6b628aa52b8b
Kristiane Westgård <kristianeaw@gmail.com>
2018-05-08 21:21:05 +0200

    [BUGFIX] wrong import

--- BilparkApp/src/reducers/registerCar.js
+++ BilparkApp/src/reducers/registerCar.js
@@ -1,5 +1,6 @@
 import { GET_CAR_FAILURE, GET_CAR_REQUEST, GET_CAR_SUCCESS, GET_CAR_DECLINE,
-  GET_CAR_ACCEPT, GET_CAR_SAVE_FAILURE, GET_CAR_FORM_VALUE, RESET_GET_CAR } from '../actions/registerCar';
+  GET_CAR_ACCEPT, GET_CAR_SAVE_FAILURE, GET_CAR_FORM_VALUE } from '../actions/registerCar';
+import { RESET_GET_CAR } from '../actions/auth';
 
 export const initialState = {
   hasErrored: '',
