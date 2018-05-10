882569b3f8c1d251cce2c1604d4dc7d7b89520ef
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-05-10 11:30:16 +0200

    [Feature] Added text fields for damageReporting - TKDATA-230

--- BilparkApp/src/components/damageReport/DamageReportForm.js
+++ BilparkApp/src/components/damageReport/DamageReportForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import t from 'tcomb-form-native';
 import { connect } from 'react-redux';
 import { StyleSheet } from 'react-native';
-import { Button, Text, View } from 'native-base';
+import { Content } from 'native-base';
 
 const styles = StyleSheet.create({
 
@@ -20,22 +20,29 @@ class DamageReportForm extends Component {
     };
     const Damages = t.struct({
       KarosseriVenstre: t.Boolean,
+      KarosseriVenstreBeskrivelse: t.maybe(t.String),
       KarosseriHøyre: t.Boolean,
+      KarosseriHøyreBeskrivelse: t.maybe(t.String),
       StøtfangerFront: t.Boolean,
+      StøtfangerFrontBeskrivelse: t.maybe(t.String),
       StøtfangerBak: t.Boolean,
+      StøtfangerBakBeskrivelse: t.maybe(t.String),
       LysUtvendig: t.Boolean,
+      LysUtvendigBeskrivelse: t.maybe(t.String),
       Glass: t.Boolean,
+      GlassBeskrivelse: t.maybe(t.String),
       FelgHjul: t.Boolean,
+      FelgHjulBeskrivelse: t.maybe(t.String),
     });
     const Form = t.form.Form;
     return (
-      <View style={styles.searchContainer}>
+      <Content>
         <Form
           ref={c => this.form = c}
           type={Damages}
           options={formOptions}
         />
-      </View>
+      </Content>
     );
   }
 }
