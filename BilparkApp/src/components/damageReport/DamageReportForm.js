b1ae1fa8d3d7dbc06c20b9e4fe19854d2de1cdb1
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-05-10 12:36:42 +0200

    [Feature] Added array and filled it with form input for dispatch to backend trough action - TKDATA-230

--- BilparkApp/src/components/damageReport/DamageReportForm.js
+++ BilparkApp/src/components/damageReport/DamageReportForm.js
@@ -4,16 +4,67 @@ import { connect } from 'react-redux';
 import { StyleSheet } from 'react-native';
 import { Content } from 'native-base';
 
+import postDamageReport from '../../actions/damageReportForm';
+
 const styles = StyleSheet.create({
 
 });
 
+const Items = [
+  {
+    ItemType: 'Wheel',
+    Damaged: true,
+    Description: 'cracked or something',
+  },
+  {
+    ItemType: 'Window',
+    Damaged: true,
+    Description: 'smashed',
+  },
+  {
+    ItemType: 'CarLight',
+    Damaged: false,
+  },
+  {
+    ItemType: 'FrontBumper',
+    Damaged: false,
+  },
+  {
+    ItemType: 'BackBumper',
+    Damaged: false,
+  },
+  {
+    ItemType: 'RightBodyWork',
+    Damaged: false,
+  },
+  {
+    ItemType: 'LeftBodyWork',
+    Damaged: false,
+  },
+];
+
+
 class DamageReportForm extends Component {
   onChange(values) {
-    this.props.addValues(values);
-    console.log(values);
+    // this.props.addValues(values);
+    // console.log(values);
+    Items[0].Damaged = this.form.getValue().FelgHjul;
+    Items[0].Description = this.form.getValue().FelgHjulBeskrivelse;
+    Items[1].Damaged = this.form.getValue().Glass;
+    Items[1].Description = this.form.getValue().GlassBeskrivelse;
+    Items[2].Damaged = this.form.getValue().LysUtvendig;
+    Items[2].Description = this.form.getValue().LysUtvendigBeskrivelse;
+    Items[3].Damaged = this.form.getValue().StøtfangerBak;
+    Items[3].Description = this.form.getValue().StøtfangerBakBeskrivelse;
+    Items[4].Damaged = this.form.getValue().StøtfangerFront;
+    Items[4].Description = this.form.getValue().StøtfangerFrontBeskrivelse;
+    Items[5].Damaged = this.form.getValue().KarosseriHøyre;
+    Items[5].Description = this.form.getValue().KarosseriHøyreBeskrivelse;
+    Items[6].Damaged = this.form.getValue().KarosseriVenstre;
+    Items[6].Description = this.form.getValue().KarosseriVenstreBeskrivelse;
+    console.log(Items);
+    // this.props.changeValues(Items);
   }
-
 
   render() {
     const formOptions = {
@@ -41,6 +92,8 @@ class DamageReportForm extends Component {
           ref={c => this.form = c}
           type={Damages}
           options={formOptions}
+          value={this.props.values}
+          onChange={value => this.onChange(value)}
         />
       </Content>
     );
@@ -54,6 +107,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    // changeValues: ItemArray => dispatch(postDamageReport(ItemArray)),
   };
 };
 
