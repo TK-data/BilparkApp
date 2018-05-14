43ebe6a5d29c2f5da0c6ad0cb6ea5c2221e4e1f7
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-05-14 19:52:30 +0200

    [Feature] Added testing to the submit button in the DamageReportForm

--- BilparkApp/src/__tests__/components/damageReport/DamageReportForm-test.js
+++ BilparkApp/src/__tests__/components/damageReport/DamageReportForm-test.js
@@ -2,16 +2,35 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import t from 'tcomb-form-native';
+import { Button } from 'native-base';
 
 import DamageReportForm from '../../../components/damageReport/DamageReportForm';
+
 
 const initialState = {
   damageReportForm: {
     isLoading: false,
     hasErrored: false,
+    currentDamageReport: {},
   },
   damageReportValues: {
-    values: 'test',
+    values: {
+      KarosseriVenstre: false,
+      KarosseriVenstreBeskrivelse: '',
+      KarosseriHøyre: false,
+      KarosseriHøyreBeskrivelse: '',
+      StøtfangerFront: false,
+      StøtfangerFrontBeskrivelse: '',
+      StøtfangerBak: false,
+      StøtfangerBakBeskrivelse: '',
+      LysUtvendig: false,
+      LysUtvendigBeskrivelse: '',
+      Glass: false,
+      GlassBeskrivelse: '',
+      FelgHjul: false,
+      FelgHjulBeskrivelse: '',
+    },
   },
   auth: {
     car: 'testcar',
@@ -34,5 +53,23 @@ describe('Testing DamageReportForm', () => {
       { context: { store: mockStore(initialState) } },
     );
     expect(comp.dive()).toMatchSnapshot();
+  });
+  const wrapper = shallow(
+    <DamageReportForm />,
+    { context: { store: mockStore(initialState) } },
+  );
+  const Form = t.form.Form;
+  it('Containts the form', () => {
+    const render = wrapper.dive().dive();
+    expect(render.find(Form).exists());
+  });
+  it('Contains the register button', () => {
+    const render = wrapper.dive().dive();
+    expect(render.find(Button).exists());
+  });
+  it('Can press the register button', () => {
+    const render = wrapper.dive().dive();
+    const registerButton = render.find(Button);
+    registerButton.simulate('press');
   });
 });
