7eee195f044b890835037b314bcb87fd90d017bd
Emil Schrøder <emilps@stud.ntnu.no>
2018-05-15 16:52:50 +0200

    [Tweak] Redesigned car in slider 1 - TKDATA-249

--- BilparkApp/src/components/menu/Slide1.js
+++ BilparkApp/src/components/menu/Slide1.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet } from 'react-native';
-import { View, Button, Text } from 'native-base';
+import { StyleSheet, Dimensions } from 'react-native';
+import { View, Button, Text, H2 } from 'native-base';
 import { NavigationActions } from 'react-navigation';
+
+const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   buzzWord: {
@@ -11,14 +13,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: '5%',
   },
-  carHeader: {
-    color: 'white',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
   text: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 18,
   },
   finnBil: {
     color: 'white',
@@ -30,9 +27,36 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgb(000, 039, 118)',
+    flexDirection: 'row',
   },
   button: {
     alignSelf: 'center',
+  },
+  carContent: {
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 2,
+  },
+  content: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    width: (width - 20),
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 10,
+  },
+  textContainer: {
+    width: (width - 40),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
   },
 });
 
@@ -52,6 +76,7 @@ class Slide1 extends Component {
 
     let content = (
       <View>
+        <Text style={styles.buzzWord}>Velkommen {user.Fname}!</Text>
         <Text style={styles.finnBil}>Du har ikke registrert en bil enda.. </Text>
         <Button
           bordered
@@ -70,11 +95,30 @@ class Slide1 extends Component {
 
       if (car) {
         content = (
-          <View style={styles.carDiv}>
-            <Text style={styles.carHeader}>Din bil:</Text>
-            <Text style={styles.text}>{car.Brand} {car.Model}</Text>
-            <Text style={styles.text}>Registreringsnummer {car.RegNr}</Text>
-            <Text style={styles.text}>Forsikrigsselskap: {car.InsuranceCompany}</Text>
+          <View style={styles.container}>
+            <View>
+              <H2 style={styles.header}>DIN BIL:</H2>
+              <View style={styles.content}>
+                <View style={styles.carContent}>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.text}>REGNR:</Text>
+                    <Text style={styles.text}>{car.RegNr}</Text>
+                  </View>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.text}>MERKE:</Text>
+                    <Text style={styles.text}>{car.Brand}</Text>
+                  </View>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.text}>MODELL:</Text>
+                    <Text style={styles.text}>{car.Model}</Text>
+                  </View>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.text}>REGISTRERINGSÅR:</Text>
+                    <Text style={styles.text}>{car.RegYear}</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
           </View>
         );
       }
@@ -82,7 +126,6 @@ class Slide1 extends Component {
 
     return (
       <View style={styles.slide1}>
-        <Text style={styles.buzzWord}>Velkommen {user.Fname}!</Text>
         { content }
       </View>
     );
