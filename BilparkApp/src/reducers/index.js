31acf732abe7c70afa5b3b1411a49680b0e71a38
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-05-20 14:01:19 +0200

    [Tweak] Removed unused redux-form

--- BilparkApp/src/reducers/index.js
+++ BilparkApp/src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import nav from './navigation';
 import auth from './auth';
 import { loginMail, loginOptions } from './form';
-import form from './redux-form';
 import { carFetch, carForm } from './registerCar';
 import modals from './modals';
 import { fuelRefill } from './fuelRefill';
@@ -18,7 +17,6 @@ const RootReducer = combineReducers({
   auth,
   loginMail,
   loginOptions,
-  form,
   carFetch,
   carForm,
   registerUserModalVisible,
