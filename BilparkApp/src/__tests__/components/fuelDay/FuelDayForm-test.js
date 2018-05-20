3620c419fc8467cba592a52e97bfaba8a3f6dcea
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2018-05-20 14:14:55 +0200

    [Feature] Added working render tests for both fuelDay components

---
+++ BilparkApp/src/__tests__/components/fuelDay/FuelDayForm-test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import FuelDayForm from '../../../components/fuelDay/FuelDayForm';
+
+const initialState = {
+  hasErrored: false,
+  isLoading: true,
+  auth: {
+    hasErrored: false,
+    isLoading: false,
+    user: '{"UserID":111,"Email":"awkdnawpidn@aiwfaf.no","CompanyID":7,"CarID":82,"Address":"Oiabfoaiwbf","Fname":"Obafiobafiob","Lname":"Oabfoaib","FuelDay":0,"FuelTime":"12-00","FuelNotification":false,"createdAt":"2018-05-20T11:30:10.000Z","updatedAt":"2018-05-20T11:30:21.000Z"}',
+  },
+  modals: {
+    fuelDay: false,
+    fuelRefill: false,
+  },
+};
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Testing FuelDayForm', () => {
+  it('Renders as expected', () => {
+    const comp = shallow(
+      <FuelDayForm />,
+      { context: { store: mockStore(initialState) } },
+    );
+    expect(comp.dive()).toMatchSnapshot();
+  });
+});
